318fba4f9a09a3b6bc017a8cd206668b4d8e83d2
ShahinurShawon-238 <shahinurshawon35@gmail.com>
2021-12-06 01:32:42 +0600

    Day 2 complete

---
+++ lib/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final String name = "Sheikh Shahinur Rahman Shawon";
+  final int days = 2;
+  final String appName = "Catalog App";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("$appName"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text(
+              "Hello, It's me, $name. This is my first Flutter Application. It's my day $days with flutter"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'home.dart';
+
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
-
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Hello, It's me, Sheikh Shahinur Rahman Shawon. This is my first Flutter Application"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
