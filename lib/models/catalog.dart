d6d11c3307d17db0e45eee6db03ec6c9b5aa141f
ShahinurShawon-238 <shahinurshawon35@gmail.com>
2021-12-12 01:51:23 +0600

    day 10 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,16 @@
+class CatalogModel{
+  static final items = [Item(
+    id: 1,
+    name: "asif",
+    description: "asif mahmud akash",
+    price: 100,
+    color: "#000000",
+    image: "https://media-exp1.licdn.com/dms/image/C5603AQGhnPl1M6JH6Q/profile-displayphoto-shrink_200_200/0/1617511185416?e=1644451200&v=beta&t=8iIx9IgCIU1rNQMpuzemHOKA2mbmFXAYkpRIvrCeIQE"
+
+)];
+}
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String description;
   final num price;
@@ -16,12 +27,4 @@ class Item {
       });
 }
 
-final products = [Item(
-  id: "123",
-  name: "asif",
-  description: "asif mahmud akash",
-  price: 100,
-  color: "#000000",
-  image: "https://media-exp1.licdn.com/dms/image/C5603AQGhnPl1M6JH6Q/profile-displayphoto-shrink_200_200/0/1617511185416?e=1644451200&v=beta&t=8iIx9IgCIU1rNQMpuzemHOKA2mbmFXAYkpRIvrCeIQE"
 
-)];
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
+import 'package:first_app/models/catalog.dart';
 import 'package:first_app/widgets/drawer.dart';
+import 'package:first_app/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -10,14 +12,20 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text(appName),
       ),
-      body: Center(
-        child: Container(
-          child: Text(
-              "Hello, It's me, $name. This is my first Flutter Application. It's my day $days with flutter"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MtDrawer(),
