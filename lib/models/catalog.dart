4701ffbdc56fc2d51db1492bde116c261406d661
ShahinurShawon-238 <shahinurshawon35@gmail.com>
2021-12-11 23:25:08 +0600

    day 10 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,27 @@
+class Item {
+  final String id;
+  final String name;
+  final String description;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({
+      required this.id,
+      required this.name,
+      required this.description,
+      required this.price,
+      required this.color,
+      required this.image
+      });
+}
+
+final products = [Item(
+  id: "123",
+  name: "asif",
+  description: "asif mahmud akash",
+  price: 100,
+  color: "#000000",
+  image: "https://media-exp1.licdn.com/dms/image/C5603AQGhnPl1M6JH6Q/profile-displayphoto-shrink_200_200/0/1617511185416?e=1644451200&v=beta&t=8iIx9IgCIU1rNQMpuzemHOKA2mbmFXAYkpRIvrCeIQE"
+
+)];
